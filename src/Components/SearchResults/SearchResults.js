1268fc1b4a90a7689ad0e3820ead84ed20de37f1
JuliusPesc <peschardjulius@gmail.com>
2022-08-22 15:23:08 +0200

    Create Static Components

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,19 @@
+
+
+import React from  'react';
+import ReactDOM from 'react-dom';
+
+import './SearchResults.css';
+
+class SearchResults extends React.Component {
+    render() {
+        return (
+            <div className="SearchResults">
+                <h2>Results</h2>
+                {/* <TrackList/> */}
+            </div>    
+        )
+    }
+}
+
+export default SearchResults;
