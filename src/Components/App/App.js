db00ec556724f686b8393b8a2403b8f645438504
JuliusPesc <peschardjulius@gmail.com>
2022-08-22 16:36:45 +0200

    Remove tracks from a playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom'
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
+import { toHaveStyle } from '@testing-library/jest-dom/dist/matchers';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,6 +16,7 @@ class App extends React.Component {
       playlistTracks: [{name: 'playlistName1', artist: 'playlistArtist1', album: 'playlistAlbum1', id: 4}, {name: 'playlistName2', artist: 'playlistArtist2', album: 'playlistAlbum2', id: 5}, {name: 'playlistName3', artist: 'playlistArtist3', album: 'playlistAlbum3', id: 6} ] 
     }
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
@@ -27,6 +29,13 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks})
   }
 
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({playlistTracks: tracks})
+  }
+
   render() {
     return (
       <div>
@@ -35,7 +44,7 @@ class App extends React.Component {
           <SearchBar/>
           <div className="App-playlist">
           <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
-          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
+          <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
           </div>
         </div>
       </div>
