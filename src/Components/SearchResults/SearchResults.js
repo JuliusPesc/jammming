eddfb874a2cce49ae15608a05f0eb8b5b5db7ec7
JuliusPesc <peschardjulius@gmail.com>
2022-08-22 15:41:29 +0200

    Pass down search result and render result list

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -5,12 +5,14 @@ import ReactDOM from 'react-dom';
 
 import './SearchResults.css';
 
+import TrackList from '../TrackList/TrackList';
+
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                 <h2>Results</h2>
-                {/* <TrackList/> */}
+                <TrackList tracks={this.props.searchResults}/>
             </div>    
         )
     }
