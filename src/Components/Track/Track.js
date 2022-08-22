9dc751ad2ddf5965719e7d57d3f260da2c65543d
JuliusPesc <peschardjulius@gmail.com>
2022-08-22 16:24:51 +0200

    Add tracks to a playlist

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -5,13 +5,22 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.addTrack = this.addTrack.bind(this);  
+    }
 
     renderAction() {
         if(this.props.isRemoval) {
             return <button className="Track-action">-</button>
         } else {
-            return <button className="Track-action">+</button>
+            return <button className="Track-action" onClick={this.addTrack}>+</button>
         }
+    }
+
+    addTrack() {
+        this.props.onAdd(this.props.track);
     }
 
     render() {
